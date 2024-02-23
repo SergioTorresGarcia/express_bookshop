f9c80c9f6d40893563f89e433dbcd4be3d0b08d9
stg <a.sergiotorres@gmail.com>
2024-02-23 11:55:43 +0100

    feat: env sample

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,22 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
+app.get("/alive", (req, res) => {
+    res.send("Server is alive")
+})
+
+app.get("/heathy", (req, res) => {
+    res.status(200).json(
+        {
+            "success": true,
+            "message": "Server is healthy"
+        }
+    )
+    // res.send("Server is healthy")
+
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 
