c63d5f8750ea95c1762ff0c8cb8a6a1eea783415
stg <a.sergiotorres@gmail.com>
2024-02-23 12:24:12 +0100

    feat: create roleController & getRoles

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { getRoles } from "../controllers/roleController";
 
 dotenv.config();
 
@@ -11,17 +12,18 @@ app.get("/alive", (req, res) => {
     res.send("Server is alive")
 })
 
-app.get("/heathy", (req, res) => {
+app.get("/healthy", (req, res) => {
     res.status(200).json(
         {
             "success": true,
             "message": "Server is healthy"
         }
     )
-    // res.send("Server is healthy")
-
 })
 
+// roles routes
+
+app.get("/roles", getRoles)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
