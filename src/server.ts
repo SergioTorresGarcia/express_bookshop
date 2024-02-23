68a87c8661830b43b95b5fcf13f94d9a9a99a084
stg <a.sergiotorres@gmail.com>
2024-02-23 11:05:46 +0100

    feat:dotenv library added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Server is running");
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 
 })
