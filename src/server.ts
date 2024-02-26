2108c2ec94861100d9d688390ab5c20365391488
stg <a.sergiotorres@gmail.com>
2024-02-26 11:54:07 +0100

    feat: basic configuration for express project

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import "dotenv/config";
 import { app } from "./app";
+import { AppDataSource } from "./database/db";
 
 const PORT = process.env.PORT || 4001;
 
 
 const startServer = () => {
     // al final hay que decirle a la aplicación que se ponga en marcha y escuche a todas las rutas
-    app.listen(PORT, () => {
-        console.log(`Server is running on port: ${PORT}`);
+    AppDataSource.initialize()
+        .then(() => {
+            console.log("database connected")
 
-    })
+            app.listen(PORT, () => {
+                console.log(`Server is running on port: ${PORT}`);
+
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
 }
+
 
 startServer();
 // "npm run dev" in terminal to start the server
